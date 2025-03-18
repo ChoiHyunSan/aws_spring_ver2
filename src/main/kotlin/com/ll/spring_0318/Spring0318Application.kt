d1955ca2ee1517e9508bc7ede77a44e3873ee8e4
ChoiHyunSan <chsan626@gmail.com>
2025-03-18 10:25:52 +0900

    add github actions

--- src/main/kotlin/com/ll/spring_0318/Spring0318Application.kt
+++ src/main/kotlin/com/ll/spring_0318/Spring0318Application.kt
@@ -2,8 +2,10 @@ package com.ll.spring_0318
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cache.annotation.EnableCaching
 
 @SpringBootApplication
+@EnableCaching
 class Spring0318Application
 
 fun main(args: Array<String>) {
---
+++ src/main/kotlin/com/ll/spring_0318/global/redis/global/jpa/entity/BaseEntity.kt
@@ -0,0 +1,5 @@
+package com.ll.spring_0318.global.redis.global.jpa.entity
+
+class BaseEntity {
+
+}
