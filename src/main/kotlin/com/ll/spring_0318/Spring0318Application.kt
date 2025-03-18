754fd3c8a7291dab9709a26966e4133e972ec0c9
ChoiHyunSan <chsan626@gmail.com>
2025-03-18 09:48:44 +0900

    first commit

---
+++ src/main/kotlin/com/ll/spring_0318/Spring0318Application.kt
@@ -0,0 +1,11 @@
+package com.ll.spring_0318
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Spring0318Application
+
+fun main(args: Array<String>) {
+    runApplication<Spring0318Application>(*args)
+}
---
+++ src/test/kotlin/com/ll/spring_0318/Spring0318ApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.ll.spring_0318
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class Spring0318ApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
