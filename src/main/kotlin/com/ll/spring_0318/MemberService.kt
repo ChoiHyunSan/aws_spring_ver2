f3cad8099791ef28680a9af4460992eef383b375
ChoiHyunSan <chsan626@gmail.com>
2025-03-18 13:05:28 +0900

    fix Home Service Logic

--- src/main/kotlin/com/ll/spring_0318/MemberService.kt
+++ src/main/kotlin/com/ll/spring_0318/MemberService.kt
@@ -1,12 +1,13 @@
 package com.ll.spring_0318
 
 import org.springframework.stereotype.Service
+import java.util.concurrent.atomic.AtomicInteger
 
 @Service
 class MemberService (
-    private var memberCount : Int = 0
+    private var memberCount : AtomicInteger = AtomicInteger(0)
 ){
     fun count(): Int = plusMemberCount()
 
-    private fun plusMemberCount(): Int = memberCount++
+    private fun plusMemberCount(): Int = memberCount.incrementAndGet()
 }
